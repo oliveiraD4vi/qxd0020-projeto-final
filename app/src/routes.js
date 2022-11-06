cd0b841e8666f8c5c631fc56a493815926e63433
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-11-06 19:52:21 -0300

    feat(vehicles.vue): added vehicles view component and route

--- app/src/routes.js
+++ app/src/routes.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from './views/User/Home/Home.vue';
 import About from './views/User/About/About.vue';
+import Vehicles from './views/User/Vehicles/Vehicles.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
+  },
+  {
+    path: '/vehicles',
+    name: 'vehicles',
+    component: Vehicles
   }
 ];
 
---
+++ app/src/views/User/Vehicles/Vehicles.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Vehicles</h1>
+</template>
+
+<style src="./Vehicles.scss" lang="scss" scoped />
