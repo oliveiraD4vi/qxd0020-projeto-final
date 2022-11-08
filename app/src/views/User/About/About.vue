eb261210ebbd471c6bbd419e12a6c978c97a0c59
Robson <robsonad07@gmail.com>
2022-11-08 14:51:35 -0300

    feat(About): modified About content

--- app/src/views/User/About/About.vue
+++ app/src/views/User/About/About.vue
@@ -1,5 +1,22 @@
-<script setup></script>
-
 <template>
-  <h1>About</h1>
+  <div className="page-container about-container">
+    <img src="../../../assets/about-us.svg" alt="man standing" width="400" height="400" />
+    <div id="message">
+      <h1>Olá!</h1>
+      <p>
+        Este é um projeto para a disciplina de Desenvolvimento de Software
+        para Web. Visite{{" "}}
+        <a
+          href="https://github.com/oliveiraD4vi/vehicle-rental-system-web"
+          target="_blank"
+          rel="noreferrer"
+        >
+          nosso repositório
+        </a>
+        .
+      </p>
+    </div>
+  </div>
 </template>
+
+<style src="./About.scss" lang="scss" scoped></style>
