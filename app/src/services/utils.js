a59060530f82e76db3ba47e0ea03b66ef3427bee
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-11-10 15:42:44 -0300

    feat(axios): added axios library and api function that implements axios

---
+++ app/src/services/utils.js
@@ -0,0 +1,46 @@
+export const rolesPath = [
+  {
+    path: "/",
+    role: ["CLIENT"],
+  },
+  {
+    path: "/vehicles",
+    role: ["CLIENT"],
+  },
+  {
+    path: "/about",
+    role: ["CLIENT"],
+  },
+  {
+    path: "/reservations",
+    role: ["CLIENT"],
+  },
+  {
+    path: "/admin/home",
+    role: ["ADMIN"],
+  },
+  {
+    path: "/admin/users",
+    role: ["ADMIN"],
+  },
+  {
+    path: "/admin/users/data",
+    role: ["ADMIN"],
+  },
+  {
+    path: "/admin/vehicles",
+    role: ["ADMIN"],
+  },
+  {
+    path: "/admin/vehicles/data",
+    role: ["ADMIN"],
+  },
+  {
+    path: "/admin/reservations",
+    role: ["ADMIN"],
+  },
+  {
+    path: "/admin/reservations/data",
+    role: ["ADMIN"],
+  },
+];
