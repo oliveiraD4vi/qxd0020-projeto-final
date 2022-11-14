492b41f755172ae0087ee12c0f48c06e12e03569
Robson <robsonad07@gmail.com>
2022-11-14 13:46:18 -0300

    feat(home-admin-component): added home-admin-component

---
+++ app/src/components/Admin/Home.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { CarOutlined, FileOutlined, UserOutlined } from "@ant-design/icons-vue"
+
+  const menuItems = [
+    {
+      name: "Veículos",
+      components: <CarOutlined/>
+    },
+    {
+      name: "Reservas",
+      components: CarOutlined
+    },
+    {
+      name: "Usuários",
+      components: CarOutlined
+    },
+  ];
+</script>
+
+<template>
+  <div class="admin-home-container" >
+      <a class="menu-item" v-for="item in menuItems" key="item.name">
+        {{item.components}} {{item.name}}
+      </a>
+  </div>
+</template>
+
+<style src="./Home.scss" lang="scss" scoped></style>
