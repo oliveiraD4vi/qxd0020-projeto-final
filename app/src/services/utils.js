53f5dbbcfcdd33055ac547f7feed8ee231037daa
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-11-15 11:34:28 -0300

    feat(utils): added cpf validation function

--- app/src/services/utils.js
+++ app/src/services/utils.js
@@ -44,3 +44,61 @@ export const rolesPath = [
     role: ["ADMIN"],
   },
 ];
+
+export function validateCpf(rule, value) {
+  try {
+    if (!value) {
+      return Promise.resolve();
+    }
+    let cpfInput = value;
+    let cpfNumber = cpfInput.replace(/\D/g, '');
+    if (cpfNumber !== '') {
+      let validate = /^[0-9]{11}$/;
+      if (validate.test(cpfNumber)) {
+        if (
+          cpfNumber === '00000000000' ||
+          cpfNumber === '11111111111' ||
+          cpfNumber === '22222222222' ||
+          cpfNumber === '33333333333' ||
+          cpfNumber === '44444444444' ||
+          cpfNumber === '55555555555' ||
+          cpfNumber === '66666666666' ||
+          cpfNumber === '77777777777' ||
+          cpfNumber === '88888888888' ||
+          cpfNumber === '99999999999'
+        )
+          return Promise.reject('CPF inválido');
+        let sum;
+        let rest;
+        sum = 0;
+
+        for (let i = 0; i <= 9; i++) {
+          sum = sum + cpfNumber.substring(i - 1, i) * (11 - i);
+        }
+        rest = (sum * 10) % 11;
+
+        if (rest === 10 || rest === 11) {
+          rest = 0;
+        }
+        if (rest !== parseInt(cpfNumber.substring(9, 10))) {
+          return Promise.reject('CPF inválido');
+        }
+
+        sum = 0;
+        for (let i = 1; i <= 10; i++) {
+          sum = sum + cpfNumber.substring(i - 1, i) * (12 - i);
+        }
+        rest = (sum * 10) % 11;
+        if (rest === 10 || rest === 11) {
+          rest = 0;
+        }
+        if (rest !== parseInt(cpfNumber.substring(10, 11))) {
+          return Promise.reject('CPF inválido');
+        }
+      }
+    }
+    return Promise.resolve();
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
