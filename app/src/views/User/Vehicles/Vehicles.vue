f143530602536c3e2e757336571cc618082a0bb0
Robson <robsonad07@gmail.com>
2022-11-18 10:37:00 -0300

    feat(vehicles): added search component

--- app/src/views/User/Vehicles/Vehicles.vue
+++ app/src/views/User/Vehicles/Vehicles.vue
@@ -1,24 +1,22 @@
 <script setup>
   import HeaderVue from '../../../components/Header/Header.vue';
   import FooterVue from '../../../components/Footer/Footer.vue';
-  import notification from "../../../services/notifications";
-  import DateSelector from "../../../components/DateSelector/DateSelector.vue";
+  import Notification from "../../../services/notifications";
   import { onMounted } from 'vue';
   import { useState } from '../../../services/useState';
   import { api } from "../../../services/api";
   import image1 from "../../../assets/car-example-green.png";
   import image2 from "../../../assets/car-example-grey.png";
   import image3 from "../../../assets/car-example-white.png";
+  import { ArrowRightOutlined } from "@ant-design/icons-vue"
 
   const images = [image1, image2, image3];
   const [data, setData] = useState();
   const [setSearchValue] = useState(null);
   const [pagination, setPagination] = useState();
-  const [setTotalCount] = useState();
+  const [totalCount, setTotalCount] = useState();
 
-  onMounted(async () =>{
-    console.log("oi");
-
+  onMounted(async () => {
     setPagination({ page: 1, size: 4, sort: "ASC" });
     getData(1, 4, "ASC");
   });
@@ -36,7 +34,7 @@
     } catch (error) {
       const { data } = error.response;
 
-      notification("error", data.message);
+      Notification("error", data.message);
     }
   };
 
@@ -45,8 +43,13 @@
   };
 
   const onChangeSelect = (sort) => {
-    setPagination({ ...pagination, sort });
-    getData(pagination.page, pagination.size, sort);
+    //setPagination({ ...pagination, sort });
+    getData(pagination.value.page, pagination.value.size, sort);
+  };
+
+  const onChangePagination = (page, size) => {
+    //setPagination({ ...pagination, value.page: page, size });
+    getData(page, size, pagination.value.sort);
   };
 
 </script>
@@ -55,28 +58,31 @@
   <HeaderVue />
   <main class="main-container">
     <div class="cars-container">
-      <DateSelector data="reservationData" />
-
       <div class="search-container">
-        <a-search
+        <a-input-search
           class="search-input"
           placeholder="Pesquisar por marca ou modelo"
-          loading="loading"
           @on-search="(value) => handleSearch(value)"
           @on-change="(value) => handleSearch(value)"
         />
       </div>
 
       <div class="pagination-container">
-          <a-pagination
-          />
+        <a-pagination 
+          v-if="pagination != null"
+          v-model:current="pagination.page"
+          v-model:defaultPageSize="pagination.size"
+          v-model:total="totalCount"
+          show-size-changer
+          @change="onChangePagination"
+        />
 
         <div class="sorter">
           <span>Ordem:</span>
 
           <a-select default-value="ASC" @on-change="onChangeSelect">
-            <a-option value="ASC">Crescente</a-option>
-            <a-option value="DESC">Decrescente</a-option>
+            <a-select-option value="ASC">Crescente</a-select-option>
+            <a-select-option value="DESC">Decrescente</a-select-option>
           </a-select>
         </div>
       </div>
