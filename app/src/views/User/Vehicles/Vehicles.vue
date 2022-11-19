7c15df40bb34068f1a0ed703b708546d1b297a12
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-11-19 20:10:10 -0300

    feat(vehicles): added vehicles listing method

--- app/src/views/User/Vehicles/Vehicles.vue
+++ app/src/views/User/Vehicles/Vehicles.vue
@@ -1,57 +1,70 @@
 <script setup>
-  import HeaderVue from '../../../components/Header/Header.vue';
-  import FooterVue from '../../../components/Footer/Footer.vue';
-  import Notification from "../../../services/notifications";
-  import { onMounted } from 'vue';
-  import { useState } from '../../../services/useState';
-  import { api } from "../../../services/api";
-  import image1 from "../../../assets/car-example-green.png";
-  import image2 from "../../../assets/car-example-grey.png";
-  import image3 from "../../../assets/car-example-white.png";
-  import { ArrowRightOutlined } from "@ant-design/icons-vue"
+import HeaderVue from "../../../components/Header/Header.vue";
+import FooterVue from "../../../components/Footer/Footer.vue";
+import Notification from "../../../services/notifications";
 
-  const images = [image1, image2, image3];
-  const [data, setData] = useState();
-  const [setSearchValue] = useState(null);
-  const [pagination, setPagination] = useState();
-  const [totalCount, setTotalCount] = useState();
+import { onMounted, ref } from "vue";
+import { useState } from "../../../services/useState";
+import { api } from "../../../services/api";
+import { ArrowRightOutlined } from "@ant-design/icons-vue";
 
-  onMounted(async () => {
-    setPagination({ page: 1, size: 4, sort: "ASC" });
-    getData(1, 4, "ASC");
-  });
+import image1 from "../../../assets/car-example-green.png";
+import image2 from "../../../assets/car-example-grey.png";
+import image3 from "../../../assets/car-example-white.png";
 
-  const getData = async (page, size, sort) => {
-    try {
-      const response = await api.get(
-        `/vehicle/list?page=${page}&size=${size}&sort=${sort}`
-      );
+const images = [image1, image2, image3];
 
-      const { data } = response;
-      setData(data.cars);
-      setTotalCount(data.totalCount);
+const [data, setData] = useState();
+const [pagination, setPagination] = useState();
+const [totalCount, setTotalCount] = useState();
 
-    } catch (error) {
-      const { data } = error.response;
+const pageSizeOptions = ref(["5", "10", "15", "20", "30"]);
 
-      Notification("error", data.message);
-    }
-  };
+onMounted(async () => {
+  setPagination({ page: 1, size: 5, sort: "ASC", search: "" });
+  getData(1, 5, "ASC", "");
+});
 
-  const handleSearch = (e) => {
-    setSearchValue(e.target.value);
-  };
+const getData = async (page, size, sort, search) => {
+  try {
+    const response = await api.get(
+      `/vehicle/list?page=${page}&size=${size}&sort=${sort}&search=${search}`
+    );
 
-  const onChangeSelect = (sort) => {
-    //setPagination({ ...pagination, sort });
-    getData(pagination.value.page, pagination.value.size, sort);
-  };
+    const { data } = response;
+    setData(data.cars);
+    setTotalCount(data.totalCount);
+  } catch (error) {
+    const { data } = error.response;
 
-  const onChangePagination = (page, size) => {
-    //setPagination({ ...pagination, value.page: page, size });
-    getData(page, size, pagination.value.sort);
-  };
+    Notification("error", data.message);
+  }
+};
 
+const onChangeSearch = (search) => {
+  setPagination({ ...pagination.value, search: search });
+  getData(
+    pagination.value.page,
+    pagination.value.size,
+    pagination.value.sort,
+    search
+  );
+};
+
+const onChangeSelect = (sort) => {
+  setPagination({ ...pagination.value, sort });
+  getData(
+    pagination.value.page,
+    pagination.value.size,
+    sort,
+    pagination.value.search
+  );
+};
+
+const onChangePagination = (page, size) => {
+  setPagination({ ...pagination.value, page, size });
+  getData(page, size, pagination.value.sort, pagination.value.search);
+};
 </script>
 
 <template>
@@ -62,17 +75,17 @@
         <a-input-search
           class="search-input"
           placeholder="Pesquisar por marca ou modelo"
-          @on-search="(value) => handleSearch(value)"
-          @on-change="(value) => handleSearch(value)"
+          @search="(value) => onChangeSearch(value)"
         />
       </div>
 
       <div class="pagination-container">
-        <a-pagination 
+        <a-pagination
           v-if="pagination != null"
           v-model:current="pagination.page"
           v-model:defaultPageSize="pagination.size"
           v-model:total="totalCount"
+          :page-size-options="pageSizeOptions"
           show-size-changer
           @change="onChangePagination"
         />
@@ -80,7 +93,7 @@
         <div class="sorter">
           <span>Ordem:</span>
 
-          <a-select default-value="ASC" @on-change="onChangeSelect">
+          <a-select default-value="ASC" @change="onChangeSelect">
             <a-select-option value="ASC">Crescente</a-select-option>
             <a-select-option value="DESC">Decrescente</a-select-option>
           </a-select>
@@ -89,23 +102,23 @@
 
       <div class="viewer-container">
         <div class="container-listing">
-          <div v-for="item in data" v-bind:key="item.plate" class="car-container">
-            <h1>
-              {{item.brand}} {{item.model}}
-            </h1>
+          <div
+            v-for="item in data"
+            v-bind:key="item.plate"
+            class="car-container"
+          >
+            <h1>{{ item.brand }} {{ item.model }}</h1>
 
             <div class="image-container">
-              <img v-bind:src="images[Math.floor(Math.random() * 3)]" alt="car" />
+              <img
+                v-bind:src="images[Math.floor(Math.random() * 3)]"
+                alt="car"
+              />
             </div>
 
-            <a-button
-              type="primary"
-              class="offer-button"
-            >
-              <span id="value">R$ {{item.value}},00</span>
-              <span id="text">
-                OFERTA <ArrowRightOutlined />
-              </span>
+            <a-button type="primary" class="offer-button">
+              <span id="value">R$ {{ item.value }},00</span>
+              <span id="text"> OFERTA <ArrowRightOutlined /> </span>
             </a-button>
           </div>
         </div>
