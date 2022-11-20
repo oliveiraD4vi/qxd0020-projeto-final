e1d501147cc7a2c04bb039298bccd9c72209609d
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-11-19 21:27:19 -0300

    feat(routes): added base views to the admin crud screens

---
+++ app/src/views/Admin/Reservations/Reservations.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HeaderVue from "../../../components/Header/Header.vue";
+import FooterVue from "../../../components/Footer/Footer.vue";
+</script>
+
+<template>
+  <HeaderVue />
+  <main class="main-container">
+    <h1>Reservations</h1>
+  </main>
+  <FooterVue />
+</template>
+
+<style src="./Reservations.scss" lang="scss" scoped />
---
+++ app/src/views/Admin/Vehicles/Vehicles.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HeaderVue from "../../../components/Header/Header.vue";
+import FooterVue from "../../../components/Footer/Footer.vue";
+</script>
+
+<template>
+  <HeaderVue />
+  <main class="main-container">
+    <h1>Vehicles</h1>
+  </main>
+  <FooterVue />
+</template>
+
+<style src="./Vehicles.scss" lang="scss" scoped />
